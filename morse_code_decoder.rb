0e4f63a011f5fcc8ea88a48d134669e00d37e34e
jic <ji_camargo@yahoo.com>
2023-07-26 15:45:13 -0500

    fix linter Errors

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,34 +1,27 @@
-def morse_code_decode_char(morse_char)
+# frozen_string_literal: false
+
+def decode_char(morse_char)
   morse_code_dictionary = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
-    '-..' => 'D', '.' => 'E', '..-.' => 'F',
-    '--.' => 'G', '....' => 'H', '..' => 'I',
-    '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-    '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
-    '...' => 'S', '-' => 'T', '..-' => 'U',
-    '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
     '-.--' => 'Y', '--..' => 'Z'
   }
-
   alpha_char = morse_code_dictionary[morse_char]
-
-  #puts "Evaluating #{morse_char} ==> #{alpha_char}"
-
-  alpha_char ||= '' # If alpha_char is nil, set it to ''
-  alpha_char
+  alpha_char || '' # If alpha_char is nil, set it to ''
 end
 
-# decode an entire word in Morse code, takes a string parameter, 
-# and return the string representation. 
+# decode an entire word in Morse code, takes a string parameter,
+# and return the string representation.
 # Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
 def morse_code_decode_word(morse_word)
   decoded_word = ''
-  morse_chars = morse_word.split() # Split word into characters
+  morse_chars = morse_word.split # Split word into characters
   morse_chars.each do |morse_char|
-    decoded_word << morse_code_decode_char(morse_char)
+    decoded_word << decode_char(morse_char)
   end
-  return decoded_word
+  decoded_word
 end
 
 # Decode the entire message in Morse code, takes a string parameter,
@@ -40,7 +33,7 @@ def morse_code_decode_message(morse_message)
     decoded_message << morse_code_decode_word(morse_word)
     decoded_message << ' ' # Add space between words
   end
-  return decoded_message.strip # Remove trailing spaces and return the decoded message
+  decoded_message.strip # Remove trailing spaces and return the decoded message
 end
 
 # Get Morse code input from the user
@@ -48,5 +41,4 @@ puts 'Enter the Morse code message:'
 morse_code_input = gets.chomp
 
 # Call the morse_code_decoder method and print the decoded text
-decoded_msg = morse_code_decode_message(morse_code_input)
-puts 'Decoded message: #{decoded_msg}'
+puts "Decoded message: #{morse_code_decode_message(morse_code_input)}"
