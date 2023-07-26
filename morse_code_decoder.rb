7a431500ce49ddb11690cd473f487c0eeed385f8
jic <ji_camargo@yahoo.com>
2023-07-26 13:29:31 -0500

    Fix linter errors

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,20 +1,22 @@
 def morse_code_decode_char(morse_char)
   morse_code_dictionary = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', 
-    '-..' => 'D', '.' => 'E', '..-.' => 'F', 
-    '--.' => 'G', '....' => 'H', '..' => 'I', 
-    '.---' => 'J', '-.-' => 'K', '.-..' => 'L', 
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+    '-..' => 'D', '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H', '..' => 'I',
+    '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
     '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', 
-    '...' => 'S', '-' => 'T', '..-' => 'U', 
-    '...-' => 'V', '.--' => 'W', '-..-' => 'X', 
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T', '..-' => 'U',
+    '...-' => 'V', '.--' => 'W', '-..-' => 'X',
     '-.--' => 'Y', '--..' => 'Z'
   }
 
-  myChar = morse_code_dictionary[morse_char]
-  #puts "Evaluating #{morse_char} ==> #{myChar}"
-  myChar ||= '' # If myChar is nil, set it to ''
-  myChar
+  alpha_char = morse_code_dictionary[morse_char]
+
+  #puts "Evaluating #{morse_char} ==> #{alpha_char}"
+
+  alpha_char ||= '' # If alpha_char is nil, set it to ''
+  alpha_char
 end
 
 # decode an entire word in Morse code, takes a string parameter, 
@@ -22,7 +24,7 @@ end
 # Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
 def morse_code_decode_word(morse_word)
   decoded_word = ''
-  morse_chars = morse_word.split(' ') # Split word into characters
+  morse_chars = morse_word.split() # Split word into characters
   morse_chars.each do |morse_char|
     decoded_word << morse_code_decode_char(morse_char)
   end
@@ -42,9 +44,9 @@ def morse_code_decode_message(morse_message)
 end
 
 # Get Morse code input from the user
-puts "Enter the Morse code message:"
+puts 'Enter the Morse code message:'
 morse_code_input = gets.chomp
 
 # Call the morse_code_decoder method and print the decoded text
 decoded_msg = morse_code_decode_message(morse_code_input)
-puts "Decoded message: #{decoded_msg}"
+puts 'Decoded message: #{decoded_msg}'
